be54b002e7336682755037ac61f9ae50831b0b3d
Vinicius <79119297+vinivp@users.noreply.github.com>
2022-10-03 01:54:41 -0300

    Add files via upload

---
+++ savegnago/js/index.js
@@ -0,0 +1,35 @@
+/*
+Parabéns, você conseguiu inspecionar a pagina até aqui, 
+Só para descobrir o login e a senha. você é print("FODA")
+*/
+
+function Login() {
+ 
+  var done = 0;
+  var usuario = document.getElementsByName("usuario")[0].value;
+  usuario = usuario.toLowerCase();
+  var senha = document.getElementsByName("senha")[0].value;
+  seha = senha.toLowerCase();
+
+  if (usuario == "123.456.789-99" && senha == "1234") {
+    window.location = "/projects/savegnago/html/dashboard.html";
+    done = 1;
+  } else {
+    alert("Login invalido, Tente novamente!");
+    alert("Informe seus dados para continuar, caso não saiba, favor entrar em contato com o desenvolvedor do Site.");
+  }
+}
+
+function mascara(i) {
+  var v = i.value;
+
+  if (isNaN(v[v.length - 1])) {
+    // impede entrar outro caractere que não seja número
+    i.value = v.substring(0, v.length - 1);
+    return;
+  }
+
+  i.setAttribute("maxlength", "14");
+  if (v.length == 3 || v.length == 7) i.value += ".";
+  if (v.length == 11) i.value += "-";
+}
