a85c98e4eceee5d7f9eb12b9de42f2d427a92c15
Vinicius <79119297+vinivp@users.noreply.github.com>
2022-10-24 11:24:45 -0300

    Delete savegnago directory

--- savegnago/js/repositor.js
+++
@@ -1,6 +0,0 @@
-const resetBloco = document.querySelectorId("restaurarTabela");
-const bloco = document;querySelectorId("anotacaoTabela")
-
-resetBloco.addEventListener("click", (bloco) => {
-    bloco.value = ""
-})
